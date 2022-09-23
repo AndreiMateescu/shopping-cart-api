4311f47e2b7621804a5ce4c9f960204c5de88bd5
Andrei Mateescu <andrei.mateescu@icloud.com>
2022-09-23 15:58:22 +0300

    Add files via upload

---
+++ Context/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingAPI.Model;
+using System.Collections.Generic;
+
+namespace ShoppingAPI.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingAPI.Context;
+using ShoppingAPI.Model;
+
+namespace ShoppingAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public ProductsController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _dataContext.Products.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            Product foundProduct = _dataContext.Products.FirstOrDefault(p => p.Id == id);
+            if (foundProduct == null)
+            {
+                return BadRequest();
+            }
+            return Ok(foundProduct);
+        }
+
+        [HttpPost]
+        public IEnumerable<Product> CreateProduct([FromBody] Product product)
+        {
+            _dataContext.Products.Add(product);
+            _dataContext.SaveChanges();
+            return _dataContext.Products.ToList();
+        }
+
+        [HttpDelete("{id}")]
+        public IEnumerable<Product> DeleteProduct(int id)
+        {
+            Product foundProduct = _dataContext.Products.FirstOrDefault(p => p.Id == id);
+            if (foundProduct != null)
+            {
+                _dataContext.Products.Remove(foundProduct);
+                _dataContext.SaveChanges();
+            }
+            return _dataContext.Products.ToList();
+        }
+    }
+}
